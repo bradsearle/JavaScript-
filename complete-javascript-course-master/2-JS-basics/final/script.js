474ca29039701f5ffeb0597b8260a55b09a750fa
Brad Searle <basearle@gmail.com>
2019-01-07 12:22:31 -0600

    setup new tutorial

--- complete-javascript-course-master/2-JS-basics/final/script.js
+++ complete-javascript-course-master/2-JS-basics/final/script.js
@@ -235,7 +235,7 @@ console.log(drink);
 }
 
 
-*/
+
 var firstName = 'brad';
 var age = 17;
 
@@ -314,11 +314,28 @@ John and Mike both play basketball in different teams. In the latest 3 games, Jo
 2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
 3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
 
+
 4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
 5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
 
 GOOD LUCK 😀
 */
+var scoreJohn = (189 + 120 + 103) / 3;
+var scoreMike = (129 + 94 + 123) / 3;
+var scoreMary = (97 + 134 + 105) / 3;
+
+if (scoreJohn > scoreMike) {
+  console.log(
+    "John's team makgott get he hell outta here. nd he wins with " +
+      scoreJohn +
+      ' points'
+  );
+} else if (scoreMike > scoreJohn) {
+  console.log("Mike's team wins with " + scoreMike + ' points');
+} else {
+  console.log('There is a draw');
+}
+
 /*
 var scoreJohn = (189 + 120 + 103) / 3;
 var scoreMike = (129 + 94 + 123) / 3;
---
+++ scrimba/scrimba.js
@@ -0,0 +1,2 @@
+var example = true;
+console.log(example);
