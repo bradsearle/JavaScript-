00de3cfebf2415571f12f1951938f59d46f4c93f
bradsearle <basearle@gmail.com>
2020-02-07 12:16:49 -0600

    adding js

---
+++ tutorials/2020-web-development-bootcamp/scripts.js
@@ -0,0 +1,97 @@
+// var output = [];
+// var count = 1;
+
+// function fizzBuzz() {
+
+//   if (count % 3 === 0)
+
+//   output.push(count);
+//   count++;
+
+//   console.log(output);
+
+// }
+
+// var n = Math.random(); // Math.floor (n * 6) + 1
+
+// n = n * 6;
+
+// console.log(n);
+
+// name = prompt("What is your name");
+// last = prompt("Waht is your last name");
+
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor (loveScore) + 1;
+
+// if (loveScore > 70) {
+//   alert("one" + loveScore)
+
+// }
+
+// if (loveScore > 30 && loveScore <= 70){
+
+// }
+// if (loveScore <= 30){
+//   alert("bad")
+
+// }
+// else {
+//   alert("two")
+
+// }
+
+// alert("Your score is " + loveScore + "%" )
+
+//  var guestList = ["brad", "elliot"];
+
+//  var name = prompt("What is your name");
+
+//  if (guestList.includes(name)) {
+//    alert("welcome");
+//  } else {
+//    alert("Sorry" + name + ", you're not on list");
+//  }
+
+// var numbers = Math.random();
+
+// numbers = numbers * 6;
+
+// var output = [];
+// var count = 1;
+
+// function fizzBuzz(){
+//   while(count <= 100) {
+
+//  if (count % 3 === 0 && count % 5 === 0) {
+//    output.push("Fizzbuzz");
+
+//  } else if (count % 3 === 0) {
+//    output.push("Fizz");
+
+//  } else if (count % 5 === 0) {
+//    output.push("Buzz");
+
+//  } else {
+//    output.push(count);
+//  }
+// count++;
+
+// }
+
+// console.log(output);
+
+// }
+
+// var numberOfBottles = 99
+// while (numberOfBottles >= 0) {
+//     var bottleWord = "bottle";
+//     if (numberOfBottles === 1) {
+//         bottleWord = "bottles";
+//     }
+//     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+//     console.log(numberOfBottles + " " + bottleWord + " of beer,");
+//     console.log("Take one down, pass it around,");
+// 	numberOfBottles--;
+//     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+// }
